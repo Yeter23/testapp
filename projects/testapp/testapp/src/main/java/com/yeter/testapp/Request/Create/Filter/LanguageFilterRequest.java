0b2084d6f7e8891eba64af7977366d2e1d6c3341
Yeter23 <yeteryusifova00@gmail.com>
2024-10-03 14:14:49 +0400

    ele bele

--- projects/testapp/testapp/src/main/java/com/yeter/testapp/Request/Create/Filter/LanguageFilterRequest.java
+++
@@ -1,6 +0,0 @@
-package com.yeter.testapp.Request.Create.Filter;
-
-public class LanguageFilterRequest {
-    private int id;
-    private String languageName;
-}
--- projects/testapp/testapp/src/main/java/com/yeter/testapp/controller/StudentRestController.java
+++ projects/testapp/testapp/src/main/java/com/yeter/testapp/controller/StudentRestController.java
@@ -1,41 +1,40 @@
-package com.yeter.testapp.controller;
-
-import com.yeter.testapp.model.Student;
-import com.yeter.testapp.repository.StudentRepository;
-import jakarta.persistence.Id;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
-
-@RestController
-
-
-public class StudentRestController {
-    @Autowired
-    private StudentRepository studentRepository;
-
-    @GetMapping(path = "/student")
-    public Student studentObject(){
-        Student s= new Student(7,"Nane","haciyev","nane@gmail.com",509993344);
-        return s;
-    }
-    @GetMapping(path = "/students")
-    public List<Student> studentObjects(){
-        return studentRepository.findAll();
-    }
-    @PostMapping(path = "/add")
-    public void add(@RequestBody Student student){
-        student.setId(null);
-        studentRepository.save(student);
-    }
-    @PutMapping
-    public void update(@RequestBody Student student){
-        studentRepository.save(student);
-
-    }
-    @DeleteMapping(path = " /{id}")
-    public void deleteById(@PathVariable Integer id){
-        studentRepository.deleteById(id);
-    }
-}
+//package com.yeter.testapp.controller;
+//
+//import com.yeter.testapp.model.Student;
+//import com.yeter.testapp.repository.StudentsRepository;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.web.bind.annotation.*;
+//
+//import java.util.List;
+//
+//@RestController
+//
+//
+//public class StudentRestController {
+//    @Autowired
+//    private StudentsRepository studentRepository;
+//
+//    @GetMapping(path = "/student")
+//    public Student studentObject(){
+//        Student s= new Student(7,"Nane","haciyev","nane@gmail.com",509993344);
+//        return s;
+//    }
+//    @GetMapping(path = "/students")
+//    public List<Student> studentObjects(){
+//        return studentRepository.findAll();
+//    }
+//    @PostMapping(path = "/add")
+//    public void add(@RequestBody Student student){
+//        student.setId(null);
+//        studentRepository.save(student);
+//    }
+//    @PutMapping
+//    public void update(@RequestBody Student student){
+//        studentRepository.save(student);
+//
+//    }
+//    @DeleteMapping(path = " /{id}")
+//    public void deleteById(@PathVariable Integer id){
+//        studentRepository.deleteById(id);
+//    }
+//}
