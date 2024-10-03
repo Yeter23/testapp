451df9a907025e1877e208fef2114cbc777d0bfb
Yeter23 <yeteryusifova00@gmail.com>
2024-10-03 14:12:43 +0400

    Enums yaratmaq

---
+++ projects/testapp/testapp/src/main/java/com/yeter/testapp/enums/LanguageEnum.java
@@ -0,0 +1,7 @@
+package com.yeter.testapp.enums;
+
+public enum LanguageEnum {
+    AZERBAYCAN_DILI,
+    INGILIS_DILI,
+    RUS_DILI
+}
---
+++ projects/testapp/testapp/src/main/java/com/yeter/testapp/enums/LevelEnum.java
@@ -0,0 +1,7 @@
+package com.yeter.testapp.enums;
+
+public enum LevelEnum {
+    ASAN,
+    ORTA,
+    CETIN
+}
